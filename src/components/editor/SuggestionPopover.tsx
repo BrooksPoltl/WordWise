fd3a12248ecaa705496fed14056e0b9837f330ac
Brooks Poltl <bpoltl1@gmail.com>
2025-06-19 03:51:14 -0500

    Refactor DocumentEditor and TextEditor components to enhance spell-checking UI; remove unused code and streamline layout following the removal of SuggestionSidebar. Improve overall code organization and maintainability.

---
+++ src/components/editor/SuggestionPopover.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface SuggestionPopoverProps {
+  // TODO: Define props
+}
+
+const SuggestionPopover: React.FC<SuggestionPopoverProps> = () => (
+  <div className="suggestion-popover">
+    <p>Suggestion Popover</p>
+  </div>
+);
+
+export default SuggestionPopover; 
