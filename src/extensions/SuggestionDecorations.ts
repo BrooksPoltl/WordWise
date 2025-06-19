64b6c652037be2d6706da488c05d43cb554a5158
Brooks Poltl <bpoltl1@gmail.com>
2025-06-19 03:53:24 -0500

    Update package dependencies to include @tiptap/core version 2.14.0 in package.json and package-lock.json for enhanced editor functionality.

---
+++ src/extensions/SuggestionDecorations.ts
@@ -0,0 +1,7 @@
+import { Extension } from '@tiptap/core';
+
+export const SuggestionDecorations = Extension.create({
+  name: 'suggestionDecorations',
+
+  // TODO: Add logic for applying decorations based on suggestions.
+}); 
