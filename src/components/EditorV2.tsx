986931a3636a9da3298ea3b2d24ef78682323548
Brooks Poltl <bpoltl1@gmail.com>
2025-06-21 23:58:32 -0500

    Remove placeholder content from CodeMirrorEditor in EditorV2 component to streamline initial setup. This change enhances clarity and focuses on suggestion handling without unnecessary text clutter.

--- src/components/EditorV2.tsx
+++ src/components/EditorV2.tsx
@@ -4,16 +4,6 @@ import { SuggestionToggles } from './editor/SuggestionToggles';
 
 const CodeMirrorEditor = lazy(() => import('./editor/CodeMirrorEditor'));
 
-const PLACEHOLDER_CONTENT = `this is a test of the emergancy broadcast system. This is a mispelled word. This is better then that. I go to the gym everyday. Your right, we should test everything. The dog wagged it's tail. Lets go to the park and see the the ducks.
-
-The rain will effect our plans, but despite of the weather, we must continue. It is not save to swim here. My shoes are to loose. I am confidant we will succeed. The team was torn a part by the disagreement. That area is very touristic. He was amongst friends. I will stay here for awhile.
-
-I have a apple. I want to walking. I should of known better. Can you transition this for me? Please check the web site. I will see you anytime. The car of my friend is red. I will ask to him.
-
-I like apples, bananas and oranges. He said, "This is a test". We sell books, CDs, and vinyl records. We also sell books, CDs and vinyl records. There are too many  spaces here. Wait for it.... This is a very good test - a simple one.
-
-I think we should probably proceed with the evaluation. Due to the fact that it was late, we decided to make a decision to go home. This is a very long and rambling sentence that goes on for quite a while without getting to the point, which is really just to demonstrate that the long sentence detector is working as intended and will flag this particular piece of text. If we release this now, we might shoot ourselves in the foot. What the hell is going on?`;
-
 const EditorV2 = () => {
   const suggestionStore = useSuggestionStore();
 
@@ -44,8 +34,7 @@ const EditorV2 = () => {
           <div className="bg-white rounded-lg shadow-md p-4">
             <div className="border rounded-lg overflow-hidden">
               <Suspense fallback={<div>Loading Editor...</div>}>
-                <CodeMirrorEditor 
-                  initialContent={PLACEHOLDER_CONTENT}
+                <CodeMirrorEditor
                   suggestionStore={suggestionStore}
                 />
               </Suspense>
